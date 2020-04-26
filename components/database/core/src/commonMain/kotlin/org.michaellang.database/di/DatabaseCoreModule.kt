6f2c887137f40bde69804e7c8550342e9364a9a4
Michael Tarasiuk <mykhailo.tarasiuk@apico.ee>
2020-04-26 14:31:44 +0300

    tmp

--- components/database/core/src/commonMain/kotlin/org.michaellang.database/di/DatabaseCoreModule.kt
+++ components/database/core/src/commonMain/kotlin/org.michaellang.database/di/DatabaseCoreModule.kt
@@ -1,3 +1,18 @@
 package org.michaellang.database.di
 
-class DatabaseCoreModule
+import com.squareup.sqldelight.db.SqlDriver
+import org.kodein.di.Kodein
+import org.kodein.di.erased.bind
+import org.kodein.di.erased.singleton
+
+abstract class DatabaseCoreModule {
+    val module = Kodein.Module("core_database_module") {
+        import(DatabaseSQLDelightModule().module)
+        import(DatabaseDaoModule().module)
+
+        bind<SqlDriver>() with singleton { getSqlDriver() }
+
+    }
+
+    abstract fun getSqlDriver(): SqlDriver
+}
